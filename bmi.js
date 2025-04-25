12dbe718a089611d6a079ab94693ee160a9ad5d3
Zonaisha Fatima <fatimazonaisha@gmail.com>
2025-04-25 13:19:42 +0500

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,7 @@
+let height = parseFloat(prompt('Enter Height in m'))
+console.log('Height = '+height);
+let weight = parseInt(prompt('Enter Weight in kgs'))
+console.log('Weight = '+weight);
+bmi = weight/ (height * height)
+console.log("BMI : "+bmi);
+
